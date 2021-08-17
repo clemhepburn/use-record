dbb9c4bdad18c617eb3939e9cf94cc1ff83189b5
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-08-17 11:38:38 -0700

    create reducer

---
+++ src/state/reducer.js
@@ -0,0 +1,36 @@
+import { UNDO, REDO, RECORD } from './actions.js';
+
+export const initialState = {
+  before: [],
+  current: '#ff0000',
+  after: []
+};
+
+export const reducer = (state, action) => {
+  const { before, current, after } = state;
+
+  switch(action.type) {
+    case UNDO:
+      return {
+        ...state,
+        after: [current, ...after],
+        curent: before[before.length - 1],
+        before: before.slice(0, -1)
+      };
+    case REDO:
+      return {
+        ...state,
+        before: [...before, current],
+        current: after[0],
+        after: after.slice(1)
+      };
+    case RECORD:
+      return {
+        ...state,
+        before: [...before, current],
+        current: action.payload
+      };
+    default:
+      return state;
+  }
+};
