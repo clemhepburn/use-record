7ade0f1e229b2ed44f2aba82fc09f87b1a9812c5
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-08-16 19:40:04 -0700

    display, input, and both button tests passing

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
-  afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+    render(<App />);
+
+    const display = screen.getByTestId('display');
+    expect(display).toHaveStyle({ backgroundColor: '#ff0000' });
+
+    const bgColorInput = screen.getByTestId('bg-color');
+    fireEvent.change(bgColorInput, { target: { value: '#090909' } });
+    expect(display).toHaveStyle({ backgroundColor: '#090909' });
+
+    const undoButton = screen.getByTestId('undo');
+    fireEvent.click(undoButton);
+    expect(display).toHaveStyle({ backgroundColor: '#ff0000' });
+
+    const redoButton = screen.getByTestId('redo');
+    fireEvent.click(redoButton);
+    expect(display).toHaveStyle({ backgroundColor: '#090909' });
+
+  
   });
 });
